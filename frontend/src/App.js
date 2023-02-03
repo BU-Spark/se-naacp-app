8c271a90a95bf8fa4c9fea3ff33f4c41fc0cdeb2
Zachary <75510412+Zachary1575@users.noreply.github.com>
2023-02-03 16:03:47 -0500

    Cleanup | Added Routing and made App.js our bootstrap file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,45 +1,13 @@
 import './App.css';
-import SearchFields from './components/SearchFields/SearchFields.js'
-import ArticlesCard from './components/ArticleCard/ArticleCard';
-import NeighborhoodCard from './components/Neighborhood/NeighborhoodCard';
-import { Grid } from '@mui/material';
-import { TopNavBar } from './components/TopNavBar/TopNavBar';
-import { NeighborhoodDemoBoard } from './components/NeighborhoodDemoBoard/NeighborhoodDemoBoard';
-import { TrendBoard } from './components/TrendBoard/TrendBoard';
-import Read from './components/DataRetrieve/data';
-import Logo from "./logo.svg";
+
+// Main navigator
+import MainNavigator from './routes/MainNavigator'
 
 function App() {
   return (
-    <div className="App">
-      <TopNavBar></TopNavBar>
-      <Read></Read>
-      <Grid container spacing={1}>
-        <Grid item xs={3}>
-          <img style={{marginLeft: 10, marginTop: 10, width: 150}}src={Logo}></img>
-        </Grid>
-        <Grid item xs={9}>
-        <SearchFields></SearchFields>
-        </Grid>
-      </Grid>
-      <br></br>
-
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <NeighborhoodCard></NeighborhoodCard>
-        </Grid>
-        <Grid item xs={6}>
-          <TrendBoard></TrendBoard>
-        </Grid>
-        <Grid item xs={6}>
-          <NeighborhoodDemoBoard></NeighborhoodDemoBoard>
-        </Grid>
-        <Grid item xs={6}>
-        <ArticlesCard style={{marginLeft: 20}} topic="Technology"></ArticlesCard>
-        </Grid>
-
-      </Grid>
-    </div>
+    <>
+      <MainNavigator />
+    </>
   );
 }
 
---
+++ frontend/src/routes/MainNavigator.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+// Pages
+import Home from '../pages/Home'
+
+export default function MainNavigatot() {
+    return(
+    <>
+        <BrowserRouter>
+            <div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/about" element={<About />} /> */}
+            </Routes>
+            </div>
+        </BrowserRouter>
+    </>
+    );
+    
+}
