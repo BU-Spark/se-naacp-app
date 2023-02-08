9afc1144b0602c9b159ba25545fc6f6a0bdd12ff
Daniel Gonzalez <dagon@bu.edu>
2023-02-08 18:27:18 -0500

    Changed some mock data values

--- frontend/src/components/NeighborhoodDemoBoard/NeighborhoodDemoBoard.js
+++ frontend/src/components/NeighborhoodDemoBoard/NeighborhoodDemoBoard.js
@@ -20,32 +20,12 @@ export const NeighborhoodDemoBoard = ({ data /* see data tab */ }) => (
                 'asian',
                 'hispanic'
             ]}
-            indexBy="country"
+            indexBy="race"
             margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
             padding={0.3}
             valueScale={{ type: 'linear' }}
             indexScale={{ type: 'band', round: true }}
             colors={{ scheme: 'nivo' }}
-            defs={[
-                {
-                    id: 'dots',
-                    type: 'patternDots',
-                    background: 'inherit',
-                    color: '#38bcb2',
-                    size: 4,
-                    padding: 1,
-                    stagger: true
-                },
-                {
-                    id: 'lines',
-                    type: 'patternLines',
-                    background: 'inherit',
-                    color: '#eed312',
-                    rotation: -45,
-                    lineWidth: 6,
-                    spacing: 10
-                }
-            ]}
             axisTop={null}
             axisRight={null}
             axisBottom={{
