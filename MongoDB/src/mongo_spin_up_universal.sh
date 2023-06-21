c818c0d816f0345f41509c1d668d3b4a35b812a5
Nawafds <60396177+Nawafds@users.noreply.github.com>
2023-06-21 18:43:33 +0300

    Revert "Merge branch 'mongo_graphQL_migration' into Nawaf's-playaround-branch"
    This reverts commit a0f00825faaad8f015f8579b53a923eba4939848, reversing
    changes made to 1fb5e0e297e3332106bec957b750483c1ef609e1.

--- MongoDB/src/mongo_spin_up_universal.sh
+++ MongoDB/src/mongo_spin_up_universal.sh
@@ -1,20 +1,3 @@
-echo '  ________  ___  ___'     
-echo ' |\   __  \|\  \|\  \'    
-echo ' \ \  \|\ /\ \  \ \  \'   
-echo '  \ \   __  \ \  \ \  \'  
-echo '   \ \  \|\  \ \  \_\  \' 
-echo '    \ \_______\ \_______\'
-echo '     \|_______|\|_______|'
-
-echo ' ________  ________  ________  ________  ___  __'       
-echo '|\   ____\|\   __  \|\   __  \|\   __  \|\  \|\  \'      
-echo '\ \  \___|\ \  \|\  \ \  \|\  \ \  \|\  \ \  \/ /|\'     
-echo ' \ \_____  \ \   ____\ \   __  \ \   _  _\ \   ___ \'    
-echo '  \|____|\  \ \  \___|\ \  \ \  \ \  \ \  \\ \  \\ \  \'   
-echo '    ____\_\  \ \__\    \ \__\ \__\ \__\ \ _\\ \__\\ \__\'   
-echo '   |\_________\|__|     \|__|\|__|\|__|\|__|\|__| \|__|' 
-echo '   \|_________|'
-
 echo "Running Mongo Spin Up Script! Please make sure Docker Dameon is running."
 echo "Please wait as Mongo Community Edition is spinning up on docker!"
 docker pull mongo 
--- express_graphql/src/services/query_service.js
+++ express_graphql/src/services/query_service.js
@@ -18,32 +18,18 @@ const querySchema = buildSchema(`
     queryArticleKeys(
       articleData: [String]
     ): String
-    queryCensusData(
-      dateFrom: Int, 
-      dateTo: Int,
-      censusTract: String
-    ): String
   }
 `);
 
 
 const queryResolver = {
-  // queryDateAndNeighborhood (Selects by associated neighborhood and date)
-  // PARAMETERS:
-  // ------------------------
-  // <-- (neighborhood: string)
-  // <-- (date from: int)
-  // <-- (date to: Int)
-  //
-  // RETURNS:
-  // ------------------------
-  // --> (All demographic and topical data associated with neighborhood and date parameters)
+  // QUERY DATE AND NEIGHBORHOOD
   queryDateAndNeighborhood: async ({dateFrom, dateTo, neighborhood}) => { // Remember to use the deconstructor operator
     await client.connect();
     infoLogger("[queryDateAndNeighborhood]", `Querying Pipeline with parameters: dateFrom: ${dateFrom}, dateTo: ${dateTo}, neighborhood: ${neighborhood}`);
 
-    const SetOfArticleWithAssociatedTracts = new Set();
-    const SetOfArticleDataTract = new Set();
+    const SetOfArticleDataDates = new Set();
+    const SetOfArticleDataNeigh = new Set();
 
     try {  
       const db = client.db(dbName);
@@ -87,20 +73,20 @@ const queryResolver = {
         }
 
         let datesData = _res[0];
-        let tractArticles = _res[1][0].articles;
+        let neighArticles = _res[1][0].articles;
 
         for (let i = 0; i < datesData.length; i++) {
           let dateArticles = datesData[i].articles;
             for (let j = 0; j < dateArticles.length; j++) {
-              SetOfArticleWithAssociatedTracts.add(dateArticles[j]);
+              SetOfArticleDataDates.add(dateArticles[j]);
             } 
         };
 
-        for (let i = 0; i < tractArticles.length; i++) {
-          SetOfArticleDataTract.add(tractArticles[i]);
+        for (let i = 0; i < neighArticles.length; i++) {
+          SetOfArticleDataNeigh.add(neighArticles[i]);
         }
 
-        let ArrayIntersectArticles = Array.from(new Set([...SetOfArticleWithAssociatedTracts].filter((x) => SetOfArticleDataTract.has(x))));
+        let ArrayIntersectArticles = Array.from(new Set([...SetOfArticleDataDates].filter((x) => SetOfArticleDataNeigh.has(x))));
         infoLogger("[queryDateAndNeighborhood]", `The amount of intersected articles is ${ArrayIntersectArticles.length} articles.`);
 
         let documentTopicsCount = await topicsCollection.countDocuments().catch((e) => {
@@ -239,14 +225,7 @@ const queryResolver = {
     return queryResult;
   },
 
-  // queryArticleKeys (Selects by Articles)
-  // PARAMETERS:
-  // ------------------------
-  // <-- (Article Array)
-  //
-  // RETURNS:
-  // ------------------------
-  // --> (Specific Demographic information & topics of the series of articles)
+  // QUERY ARTICLES
   queryArticleKeys: async ({articleData}) => {
     await client.connect();
     infoLogger("[queryArticleKeys]", `Querying Pipeline with parameters: articleData: ${articleData}`);
@@ -276,220 +255,12 @@ const queryResolver = {
 
       // Lot more to be done... 
     } catch(e) {
-      console.log("Error:", e); // Need to resolve this to an error logger
+      console.log("Error:", e);
     }
   },
-
-  // queryCensusData (Selects by Census Tracts)
-  // PARAMETERS:
-  // ------------------------
-  // <-- (Date)
-  // <-- (Census Tract)
-  //
-  // RETURNS:
-  // ------------------------
-  // --> (Specific Demographic information & topics of that census)
-  queryCensusData: async ({dateFrom, dateTo, censusTract}) => {
-    await client.connect();
-    infoLogger("[queryCensusData]", `Querying Pipeline with parameters: dateFrom: ${dateFrom}, dateTo: ${dateTo}, censusTract: ${censusTract}`);
-
-    const SetOfArticleWithAssociatedTracts = new Set();
-    const SetOfArticleDataTract = new Set();
-
-    try {
-      const db = client.db(dbName);
-      const tractCollection = db.collection("tracts_data");
-      const datesCollection = db.collection("dates_data");
-      const topicsCollection = db.collection("topics_data");
-      const articleCollection = db.collection("articles_data");
-
-      const dateCursor = datesCollection.find(
-        {'$and': [
-            {
-              'dateSum': {
-                '$gte': dateFrom
-              }
-            }, {
-              'dateSum': {
-                '$lte': dateTo
-              }
-            }
-          ]
-        }
-      );
-  
-      const tactCursor = tractCollection.find(
-        {
-          'tract': censusTract
-        }
-      );
-
-      const queryResult = await Promise.all([dateCursor.toArray(), tactCursor.toArray()]).then( async(_res) => {
-        if (_res[0].length == 0  || _res[1].length == 0) { // Will write a better guard
-          warningLogger(
-            "[queryCensusData]", 
-            `One of the Query rendered empty. 
-            Cannot perform Query further. 
-            Please check Query Parameters, returning empty array.
-            Dates: ${_res[0].length} Neighborhood: ${_res[1].length}`
-          );
-          return;
-        }
-
-        let datesData = _res[0];
-        let tractArticles = _res[1][0].articles;
-
-        for (let i = 0; i < datesData.length; i++) {
-          let dateArticles = datesData[i].articles;
-            for (let j = 0; j < dateArticles.length; j++) {
-              SetOfArticleWithAssociatedTracts.add(dateArticles[j]);
-            } 
-        };
-
-        for (let i = 0; i < tractArticles.length; i++) {
-          SetOfArticleDataTract.add(tractArticles[i]);
-        }
-
-        let ArrayIntersectArticles = Array.from(new Set([...SetOfArticleWithAssociatedTracts].filter((x) => SetOfArticleDataTract.has(x))));
-        infoLogger("[queryDateAndNeighborhood]", `The amount of intersected articles is ${ArrayIntersectArticles.length} articles.`);
-
-        let documentTopicsCount = await topicsCollection.countDocuments().catch((e) => {
-          console.log("[MongoDB Error] Document count failed:", e);
-        });
-
-        let queryRes = [];
-        let tractObjs = [];
-        let topicsCount = new Array(documentTopicsCount).fill(0); 
-      
-        // LIST OF ALL MONGODB QUERY OBJECTS:
-        // Given a article i in Array of Intersection of Articles.
-        //
-        // tract_data: All Census tract data related to a given i
-        // topics_data: Gets the count of how many i's fall into a respective category
-        for (let i = 0; i < ArrayIntersectArticles.length; i++) {
-          let article_id = ArrayIntersectArticles[i];
-
-          tractObjs = await tract_data(tractCollection, articleCollection, article_id, tractObjs);
-          topicsCount = await topics_data(topicsCollection, article_id, topicsCount);
-        }
-        
-        let tractData = {
-          pipeline: "articleTracts",
-          data: tractObjs,
-        };
-
-        let topicsData = {
-          pipeline: "topics_data",
-          data: topicsCount,
-        }
-
-        queryRes.push(tractData, topicsData);
-
-        const dataResult = Promise.allSettled(queryRes).then((res) => {
-          let censusStringSet = new Set();
-          let data = {
-            demographics: {
-              counties: [],
-              Total_Population: 0,
-              Total_H_and_L: 0,
-              Total_not_H_and_L: 0,
-              Other_Pop: 0,
-              White: 0,
-              Black: 0,
-              American_Indian: 0,
-              Asian: 0,
-              Pacific_Islander: 0,
-              Other: 0
-            }
-          };
-        
-          for (const resPromise of res) {
-            if (resPromise.status === "fulfilled") {
-              pipeline = resPromise.value.pipeline;
-              pipelineData = resPromise.value.data;
-
-              switch(pipeline) {
-                case "articleTracts":
-                  let template = {
-                    counties: [],
-                    Total_Population: [],
-                    Total_H_and_L: [],
-                    Total_not_H_and_L: [],
-                    Other_Pop: [],
-                    White: [],
-                    Black: [],
-                    American_Indian: [],
-                    Asian: [],
-                    Pacific_Islander: [],
-                    Other: []
-                  };
-
-                  for (let i = 0; i < pipelineData.length; i++) {
-                    let dataVals = pipelineData[i];
-                    let dataString = JSON.stringify(dataVals)
-                    if (!(censusStringSet.has(dataString))) {
-                      let demographics = dataVals['demographics'];
-                      censusStringSet.add(dataString);
-
-                      template.counties.push(dataVals['county_name']);
-                      template.Total_Population.push(parseInt(demographics['p2_001n']));
-                      template.Total_H_and_L.push(parseInt(demographics['p2_002n']));
-                      template.Total_not_H_and_L.push(parseInt(demographics['p2_003n']));
-                      template.Other_Pop.push(parseInt(demographics['p2_004n']));
-                      template.White.push(parseInt(demographics['p2_005n']));
-                      template.Black.push(parseInt(demographics['p2_006n']));
-                      template.American_Indian.push(parseInt(demographics['p2_007n']));
-                      template.Asian.push(parseInt(demographics['p2_008n']));
-                      template.Pacific_Islander.push(parseInt(demographics['p2_009n']));
-                      template.Other.push(parseInt(demographics['p2_010n']))                      }
-                  };
-              
-                  const average = (array) => array.reduce((a, b) => a + b) / array.length;
-
-                  for (const key of Object.keys(template)){
-                    if (key == 'counties'){
-                      data['demographics'].counties = template.counties
-                      continue;
-                    };
-                    if (template[key].length === 0){
-                      continue;
-                    };
-                    data['demographics'][key] = Math.round(average(template[key]));
-                  };
-                  // console.log("Data after mean: ", data);
-                  break;
-                case "topics_data":
-                  data["topicsData"] = pipelineData
-                  break;
-                default:
-                  console.log("PIPELINE NOT FOUND", pipeline)// Print an error
-              };
-              data["articleData"] = ArrayIntersectArticles; // Add the articles associated
-            } else {
-              console.log("A pipeline failed")
-              // We could write a retry...
-            };
-          };
-
-          return data;
-        });
-
-        let resolvedData = Promise.resolve(dataResult).then((r) => {
-          return r;
-        });
-
-        return resolvedData;
-      });
-
-      return (Promise.resolve(queryResult).then(r => {return JSON.stringify(r)}));
-    } catch(e) {
-      console.log("Error:", e); // Need to resolve this to an error logger
-    }
-  },
-
 };
 
-// ================= Private Methods =================
+// =========================================================================================================
 
 // Private Method
 var topics_data = async(topicsCollection, article_id, topicsCount) => {
@@ -520,6 +291,7 @@ var topics_data = async(topicsCollection, article_id, topicsCount) => {
   return topicsCount;
 };
 
+// Private Method
 var tract_data = async(tractCollection, articleCollection, article_id, tractObjs) => {
   let article_data = articleCollection.find(
     {
