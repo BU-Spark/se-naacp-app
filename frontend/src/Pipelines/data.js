0b6632da08eb2e58826e3988e29fa406ddd3111e
Zachary <75510412+Zachary1575@users.noreply.github.com>
2023-06-21 09:55:03 -0400

    Fixed Census tract

--- frontend/src/Pipelines/data.js
+++ frontend/src/Pipelines/data.js
@@ -9,7 +9,7 @@ const queryURI = 'http://localhost:4000/queryValues' // Will be automated
 const clientQuery = new ApolloClient({
 	uri: queryURI,
 	cache: new InMemoryCache(),
-  });
+});
 
 // GET request to query given valid date and neighborhood
 // ======================================================
@@ -50,6 +50,44 @@ const getgetNeighborhoodAndDateData = async(dateFrom, dateTo, neighborhood) => {
 	return neigh_date_data;
 };
 
+// GET request to query given valid date and neighborhood
+// ======================================================
+// Using Axios Params to send specification
+// <-- dateFrom: start date, Date.JS object
+// <-- dateTo: end date, Date.JS object
+// <-- Neighborhood: Neighborhood as a string
+// ======================================================
+// --> Returns a resolved void promise
+const getCensusDateData = async(dateFrom, dateTo, census) => {
+	let formattedDateFrom = parseInt(dayjs(dateFrom).format('YYYYMMDD'));
+	let formattedDateTo = parseInt(dayjs(dateTo).format('YYYYMMDD'));
+	let types = [typeof 1, typeof 1, typeof ""]; // It's dirty, but javascript won't let me access the number type
+	let given = [typeof formattedDateFrom, typeof formattedDateTo, typeof census];
+
+	const QUERY = gql`query queryCensusData($formattedDateFrom: Int, $formattedDateTo: Int, $census: String) {
+		queryCensusData(dateFrom: $formattedDateFrom, dateTo: $formattedDateTo, censusTract: $census)
+	}`;
+
+	if (!typeChecker(types, given)) {
+		console.log("Killed Query before leaving!");
+		return;
+	}
+
+	let tract_data_data = await clientQuery.query({
+		query: QUERY,
+		variables: {formattedDateFrom, formattedDateTo, census}
+	}).then((_res) => {
+		return _res
+	});
+
+	// Definitely needs error checking and custom apollo type checker here...
+
+	tract_data_data = JSON.parse(tract_data_data.data.queryCensusData); // Need to change this...
+	
+
+	return tract_data_data;
+};
+
 
 // GET request to query given valid date and census tract
 // ======================================================
@@ -59,36 +97,36 @@ const getgetNeighborhoodAndDateData = async(dateFrom, dateTo, neighborhood) => {
 // <-- tract: tract as a string
 // ======================================================
 // --> Returns a resolved void promise
-const getCensusDateData = async(dateFrom, dateTo, tract) => { 
-	let formattedDateFrom = dayjs(dateFrom).format('YYYYMMDD');
-	let formattedDateTo = dayjs(dateTo).format('YYYYMMDD');
+// const getCensusDateData = async(dateFrom, dateTo, tract) => { 
+// 	let formattedDateFrom = dayjs(dateFrom).format('YYYYMMDD');
+// 	let formattedDateTo = dayjs(dateTo).format('YYYYMMDD');
 
-	const parameter_payload = {
-		dateFrom: `${formattedDateFrom}`,
-		dateTo: `${formattedDateTo}`,
-		Tract: `${tract}`
-	}
+// 	const parameter_payload = {
+// 		dateFrom: `${formattedDateFrom}`,
+// 		dateTo: `${formattedDateTo}`,
+// 		Tract: `${tract}`
+// 	}
 
-	console.log("The Parameter Payload:", parameter_payload);
+// 	console.log("The Parameter Payload:", parameter_payload);
 
-	let tract_date_data = await axios.get(
-		`http://127.0.0.1:5001/se-naacp-journalism-bias/us-central1/getCensusData`,
-		{
-			params: {
-				QueryParam: parameter_payload
-			}
-		}
-	)
-  	.then(res => {
-    	return res
-  	});
+// 	let tract_date_data = await axios.get(
+// 		`http://127.0.0.1:5001/se-naacp-journalism-bias/us-central1/getCensusData`,
+// 		{
+// 			params: {
+// 				QueryParam: parameter_payload
+// 			}
+// 		}
+// 	)
+//   	.then(res => {
+//     	return res
+//   	});
 
-	tract_date_data = tract_date_data.data;
+// 	tract_date_data = tract_date_data.data;
 
-	console.log("Tract_date_data:", tract_date_data);
+// 	console.log("Tract_date_data:", tract_date_data);
 
-	return tract_date_data;
-};	
+// 	return tract_date_data;
+// };	
 
 const getArticleData = async(articleData) => {
 	const QUERY = gql`query queryArticleKeys($articleData: [String]) {
@@ -107,27 +145,6 @@ const getArticleData = async(articleData) => {
 	return article_data;
 };
 
-
-// const getArticleData = async(articleData) => {
-// 	const parameter_payload = {
-// 		articleData: articleData
-// 	}
-
-// 	let article_data = await axios.get(
-// 		`http://127.0.0.1:5001/se-naacp-journalism-bias/us-central1/queryArticleKeys`,
-// 		{
-// 			params: {
-// 				QueryParam: parameter_payload
-// 			}
-// 		}
-// 	)
-// 	.then(res => {
-// 		return res
-// 	});
-
-// 	article_data = article_data.data;
-// 	return article_data
-// }
 
 // Private Methods
 var typeChecker = (typeArr, given) => {
