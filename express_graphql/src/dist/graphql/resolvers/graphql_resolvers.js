c7ae38c6e0624659c8a0ab8c68999c097475bec8
Nawafds <60396177+Nawafds@users.noreply.github.com>
2023-10-01 16:01:49 -0400

    code gen is up and runnning

--- express_graphql/src/dist/graphql/resolvers/graphql_resolvers.js
+++ express_graphql/src/dist/graphql/resolvers/graphql_resolvers.js
@@ -2,26 +2,40 @@ import { MongoClient } from "mongodb";
 const url = "mongodb://localhost:27017"; // Local development
 const dbName = "se_naacp_gbh";
 const client = new MongoClient(url);
+//helper function to check if a string is a numebr
+function isNumber(str) {
+    return !isNaN(str);
+}
 export const resolvers = {
     Query: {
         articleByDate: async (parent, args, contex) => {
             await client.connect();
             let db = client.db(dbName);
             const articles_data = db.collection("articles_data");
-            const getArticles = articles_data.find({
-                dateSum: {
-                    $gte: args.dateFrom,
-                    $lte: args.dateTo,
-                },
-                neighborhoods: args.neighborhood,
-            }).toArray();
-            // let queryResult = await Promise.resolve(
-            //   getArticles.toArray()
-            // ).then((res) => {
-            //   return res;
-            // });
-            console.log(getArticles);
-            return getArticles;
+            if (isNumber(args.area)) {
+                const queryResult = articles_data
+                    .find({
+                    dateSum: {
+                        $gte: args.dateFrom,
+                        $lte: args.dateTo,
+                    },
+                    tracts: args.area,
+                })
+                    .toArray();
+                return queryResult;
+            }
+            else {
+                const queryResult = articles_data
+                    .find({
+                    dateSum: {
+                        $gte: args.dateFrom,
+                        $lte: args.dateTo,
+                    },
+                    neighborhoods: args.area,
+                })
+                    .toArray();
+                return queryResult;
+            }
         },
     },
 };
--- express_graphql/src/graphql/resolvers/graphql_resolvers.ts
+++ express_graphql/src/graphql/resolvers/graphql_resolvers.ts
@@ -4,6 +4,11 @@ const url = "mongodb://localhost:27017"; // Local development
 const dbName = "se_naacp_gbh";
 const client = new MongoClient(url);
 
+//helper function to check if a string is a numebr
+function isNumber(str: any) {
+  return !isNaN(str);
+}
+
 export const resolvers = {
   Query: {
     articleByDate: async (parent, args, contex) => {
@@ -11,17 +16,31 @@ export const resolvers = {
       let db = client.db(dbName);
       const articles_data = db.collection("articles_data");
 
-      const queryResult = articles_data
-        .find({
-          dateSum: {
-            $gte: args.dateFrom,
-            $lte: args.dateTo,
-          },
-          neighborhoods: args.neighborhood,
-        })
-        .toArray();
+      if (isNumber(args.area)) {
+        const queryResult = articles_data
+          .find({
+            dateSum: {
+              $gte: args.dateFrom,
+              $lte: args.dateTo,
+            },
+            tracts: args.area,
+          })
+          .toArray();
 
-      return queryResult;
+        return queryResult;
+      } else {
+        const queryResult = articles_data
+          .find({
+            dateSum: {
+              $gte: args.dateFrom,
+              $lte: args.dateTo,
+            },
+            neighborhoods: args.area,
+          })
+          .toArray();
+
+        return queryResult;
+      }
     },
   },
 };
