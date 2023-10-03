ad9fe404867bbd4784aff01114a46c0847b17b11
Zachary <75510412+Zachary1575@users.noreply.github.com>
2023-10-03 13:26:38 -0400

    Moved MongoDB to context so we connect to it only ONCE. Added try catch to let the playground know if we cannot contact DB.

--- backend/index.ts
+++ backend/index.ts
@@ -4,13 +4,48 @@ import {startStandaloneServer}  from '@apollo/server/standalone';
 import { typeDefs }  from "./graphql/schemas/type_definitions.js";
 import { resolvers } from "./graphql/resolvers/graphql_resolvers.js";
 
+import { MongoClient, Db } from "mongodb";
+import { GraphQLError } from 'graphql';
 
+interface Context {
+  db: Db;
+}
+
+type ContextWrapperFunction = () => Promise<Context>
+
+// Apollo Server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+const connectWithMongoDB = async (mongo_url: string, db_name: string): Promise<Db> => { 
+  const client = new MongoClient(mongo_url);
+  try {
+    await client.connect();
+    return client.db(db_name);
+  } catch (error) {
+    throw new GraphQLError("Failed to connect to MongoDB", {
+      extensions: {
+        code: 'ECONNREFUSED',
+        raw_err_msg: error.message
+      }
+    });
+  }
+}
+
+// Context Wrapper
+// Build things you need inside to pass to context
+const contextWrapper: ContextWrapperFunction = async () => {
+  // Context Metadata
+  const mongo_url = "mongodb://localhost:27017"; // Local development
+  const dbName = "se_naacp_gbh";
+
+  return { db: await connectWithMongoDB(mongo_url, dbName) };
+}
+
 const { url } = await startStandaloneServer(server, {
+  context: contextWrapper,
   listen: { port: 4000 },
 });
 
