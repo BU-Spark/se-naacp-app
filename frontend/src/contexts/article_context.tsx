32f89217e2f072705b40720613134510b67adf3c
Zachary <75510412+Zachary1575@users.noreply.github.com>
2023-10-13 14:47:47 -0400

    Refactored Article Context

--- frontend/src/contexts/article_context.tsx
+++ frontend/src/contexts/article_context.tsx
@@ -4,23 +4,20 @@ import { Article } from "../__generated__/graphql"
 
 type ArticleContextType = {
     articleData: Article[] | null, // This is for components to consume
-    neighborhoodMasterList: string[] | null, // This is for upon init, think of it as getting a master list of neighborhoods
     queryArticleDataType: (queryType: any, options?: any) => void,
 }
 /* Article Queries */
+// We will pass what we need in here
 const ARTICLE_DATA_QUERY = gql`
     query articleQuery($dateFrom: Int!, $dateTo: Int!, $area: String!) {
         articleByDate(dateFrom: $dateFrom, dateTo: $dateTo, area: $area) {
-            neighborhoods
-        }
-    }
-`;
-
-// *Note* I want to make this more dynamic. Maybe a future todo... (ASK ASAD)
-const NEIGHBORHOOD_LIST_QUERY = gql`
-    query neighborhoodQuery {
-        getAllArticles {
-            neighborhoods
+            content_id
+            hl1
+            link
+            pub_date
+            position_section
+            author      
+            openai_labels
         }
     }
 `;
@@ -29,30 +26,14 @@ export const ArticleContext = React.createContext<ArticleContextType | null>(nul
 
 const ArticleProvider: React.FC = ({children}: any) => { 
     const [queryArticleData, { data: articleData, loading: articleDataLoading, error: articleDataError }] = useLazyQuery(ARTICLE_DATA_QUERY);
-    const [queryNeighborhoodData, { data: neighborhoodData, loading: neighborhoodDataLoading, error: neighborhoodDataError }] = useLazyQuery(NEIGHBORHOOD_LIST_QUERY);
 
     const [articles, setArticleData] = React.useState<Article[] | null>(null);
-    const [neighborhoods, setNeighborhoodMasterList] = React.useState<string[] | null>(null);
 
     React.useEffect(() => {
-        if (!articleData && articleDataLoading && !articleDataError) {
-            setArticleData([articleData]);
+        if (articleData && !articleDataLoading && !articleDataError) {
+            setArticleData(articleData.articleByDate);
         }
     }, [articleData, articleDataLoading, articleDataError]);
-
-    React.useEffect(() => {
-        if (neighborhoodData && !neighborhoodDataLoading && !neighborhoodDataError) {
-            let neighborhood_arr: string[] = [];
-
-            neighborhoodData.getAllArticles.forEach((elem: any) => {
-                neighborhood_arr.push(elem.neighborhoods[0]);
-            });
-
-            neighborhood_arr = removeDuplicates(neighborhood_arr);
-
-            setNeighborhoodMasterList(neighborhood_arr);
-        }
-    }, [neighborhoodData, neighborhoodDataLoading, neighborhoodDataError]);
 
     // Processor functions. (Thinking to apply them in sequence...)
     function removeDuplicates(input: string[]): string[] {
@@ -65,18 +46,12 @@ const ArticleProvider: React.FC = ({children}: any) => {
     // query Type -> Indicates which useLazyQuery hook to execute
     // options? -> (Optional) Give the parameters needed for that useLazyQeury hook
     // func_ops? -> (Optional) What functions to execute left to right to the data
-    const queryArticleDataType = (queryType: "ARTICLE_DATA" | "NEIGHBORHOOD_DATA", options?: any) => {
+    const queryArticleDataType = (queryType: "ARTICLE_DATA", options?: any) => {
         switch(queryType) {
             case "ARTICLE_DATA":
                 queryArticleData({
                     variables: options // This is where you pass in parameters
                 })
-                break;
-            case "NEIGHBORHOOD_DATA":
-
-                queryNeighborhoodData({
-                    variables: {"neighborhood": "Downtown"}
-                }) // No need for parameters, we are fetching a universal state
                 break;
             default:
                 // Something went wrong here, Needs to throw error to user...
@@ -86,7 +61,7 @@ const ArticleProvider: React.FC = ({children}: any) => {
     };
 
     return (
-        <ArticleContext.Provider value={{ articleData: articles, neighborhoodMasterList: neighborhoods, queryArticleDataType}}>
+        <ArticleContext.Provider value={{ articleData: articles, queryArticleDataType}}>
             {children}
         </ArticleContext.Provider>
     );
