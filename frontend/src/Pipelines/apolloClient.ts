a76553fbaf15fa81418b2ab1e3f4d6cd9e14f1e4
Nawafds <60396177+Nawafds@users.noreply.github.com>
2023-10-31 13:10:35 -0400

    Production ready

--- frontend/src/Pipelines/apolloClient.ts
+++ frontend/src/Pipelines/apolloClient.ts
@@ -1,9 +1,10 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 
-const deployment_uri = process.env.NAACP_DEPLOYMENT_URI;
-
+// const deployment_uri = process.env.NAACP_DEPLOYMENT_URI;
+const deployment_uri = "https://naacpbackend-production.up.railway.app"
 // const local_uri = "http://localhost:4000";
+
 const queryURI = `${deployment_uri}/queryValues`;
 const client = new ApolloClient({
   uri: queryURI,
