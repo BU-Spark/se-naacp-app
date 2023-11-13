1b094032ac65afa650d78e7a41d3328d3da74a40
Nawafds <60396177+Nawafds@users.noreply.github.com>
2023-11-13 14:45:37 -0500

    g

--- frontend/src/pages/UploadPage/RSSUploadPage.tsx
+++ frontend/src/pages/UploadPage/RSSUploadPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 import { DOMParser } from 'xmldom';
 import axios from 'axios';
 import Button from '@mui/material/Button';
