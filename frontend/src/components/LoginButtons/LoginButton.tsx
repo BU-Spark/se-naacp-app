02705efcacef3b1e71c7580a9d0736893742fd4e
Malik Baker <78194321+malbaker@users.noreply.github.com>
2023-11-14 18:57:09 -0500

    remove loadingbutton

--- frontend/src/components/LoginButtons/LoginButton.tsx
+++ frontend/src/components/LoginButtons/LoginButton.tsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "@mui/material";
 import React from "react";
-import { LoadingButton } from "@mui/lab";
 
 export const LoginButton = () => {
 	const { loginWithRedirect, isLoading } = useAuth0();
@@ -14,7 +14,7 @@ export const LoginButton = () => {
 	};
 
 	return (
-		<LoadingButton
+		<Button
 			className='button__login Button
 		'
 			onClick={handleLogin}
@@ -24,10 +24,8 @@ export const LoginButton = () => {
 				padding: ".5rem",
 				margin: " 0 1rem",
 			}}
-			loading={isLoading}
-			loadingIndicator='Loading...'
 		>
 			Log In
-		</LoadingButton>
+		</Button>
 	);
 };
