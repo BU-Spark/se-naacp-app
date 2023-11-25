f765109d2107af3929516b283fa44776c65b67d1
lixi <lxi1@binghamton.edu>
2023-11-25 01:44:48 -0500

    connect rss_feed to mongoDB

--- rssfeed/cronjob.js
+++ rssfeed/cronjob.js
@@ -3,8 +3,8 @@ const cron = require('node-cron');
 const axios = require('axios');
 const cheerio = require('cheerio');
 
-cron.schedule('* * * * *', async () => {
-  console.log('running a task every minute');
-  const url = 'https://www.wgbh.org/tags/bunp.rss';
-  console.log(await rss.scrap_data(url));
+cron.schedule('20 * * * * *', async () => {
+  console.log('running a task every 20 sec');
+  // const url = 'https://www.wgbh.org/tags/bunp.rss';
+  console.log(await rss.scrap_data(rss.get_link()));
 });
--- rssfeed/rss_feed.js
+++ rssfeed/rss_feed.js
@@ -1,37 +1,57 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const { MongoClient } = require('mongodb');
 
-const url = 'https://www.wgbh.org/tags/bunp.rss';
+const mongoUrl = "mongodb://localhost:27017"; // Local development
+const dbName = "se_naacp_gbh";
+const client = new MongoClient(mongoUrl);
 
-const scrap_data = async (url) => {
+const get_link = async () => {
+    try {
+        await client.connect();
+        let db = client.db(dbName);
+        const rss_data = db.collection("rss_data");
+        // need to change findOne in the future 
+        const rssLink = await rss_data.findOne({url: "https://www.wgbh.org/tags/bunp.rss"});
+        return rssLink.url;
+    } finally {
+        await client.close();
+    }
+}
+
+// const url = 'https://www.wgbh.org/tags/bunp.rss';
+
+const scrap_data = async () => {
     let titles = [];
+    const url = await get_link();
+    if (url) {
+        await axios.get(url).then(response => {
+            const html = response.data;
+            const $ = cheerio.load(html);
 
-    await axios.get(url).then(response => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-
-        $('item title').each(function() {
-            //console.log('>' + $(this).text()+'\n');
-            titles.push($(this).text());
+            $('item title').each(function() {
+                //console.log('>' + $(this).text()+'\n');
+                titles.push($(this).text());
+            });
+        })
+        .catch((error) => {
+            console.error('Error fetching data:', error)
         });
-    })
-    .catch((error) => {
-        console.error('Error fetching data:', error)
-    });
-
+    };
     return titles;
 }
 
 
 // Example usage
 const main = async () => {
+    let url = await get_link();
     let test = await scrap_data(url)
     console.log("TEST:\n" + test);
 }
 
 // main();
 
-module.exports={scrap_data: scrap_data};
+module.exports={scrap_data: scrap_data, get_link: get_link};
 
 
 
