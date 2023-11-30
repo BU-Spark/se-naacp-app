0b558321087317674d1bd5c95d76422b785afe0b
lixi <lxi1@binghamton.edu>
2023-11-29 23:57:39 -0500

    Added to rss scraper to scrape article titles,links,etc and write to CSV

--- rssfeed/rss_feed.js
+++ rssfeed/rss_feed.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
+const fs = require('fs');
+const path = require('path');
+const papa = require('papaparse');
+
 const { MongoClient } = require('mongodb');
 
 const mongoUrl = "mongodb://localhost:27017"; // Local development
@@ -23,33 +27,65 @@ const get_link = async () => {
 
 const scrap_data = async () => {
     let titles = [];
+    let links = [];
+    let descriptions = [];
+    let contents = [];
+    let pubDates = [];
+
     const url = await get_link();
     if (url) {
         await axios.get(url).then(response => {
             const html = response.data;
             const $ = cheerio.load(html);
 
-            $('item title').each(function() {
+            $('item').each(function() {
                 //console.log('>' + $(this).text()+'\n');
-                titles.push($(this).text());
+                titles.push($('title', this).text());
+                links.push($('guid', this).text());
+                descriptions.push($('description', this).text());
+                pubDates.push($('pubDate', this).text());
+                contents.push($('content\\:encoded', this).text());
+                // console.log($('content\\:encoded').text());
             });
         })
         .catch((error) => {
             console.error('Error fetching data:', error)
         });
     };
-    return titles;
-}
 
+    // const csvContent = titles.map((title, index) => `"${title}", "${links[index]}", "${descriptions[index]}", "${contents[index]}", "${pubDates[index]}"`).join('\n');
+    
+    const mytitle = titles.map(title => [title]);
+    const mylink = links.map(link => [link]);
+    const mydesc = descriptions.map(desc => [desc]);
+    const myDate = pubDates.map(date => [date]);
+    const myContent = contents.map(content => [content]);
+
+    // zip arrays together into a big array
+    const myarr = mytitle.map((title, index) => [title, mylink[index], mydesc[index], myDate[index], myContent[index]]);
+
+    // add headers
+    const headers = ["Title", "Link", "Description", "PubDate", "Content"];
+    const arr = [headers, ...myarr];
+
+    // papa unparse input has to be array of arrays, or array of objects.
+    const csv = papa.unparse(arr, {
+        quotes: true, // Wrap every field in quotes
+        delimiter: ",", // Use comma as the delimiter
+    });
+    // write to csv
+    fs.writeFileSync(path.resolve(__dirname, 'new_articles.csv'), csv);
+    return myarr;
+}
 
 // Example usage
 const main = async () => {
     let url = await get_link();
     let test = await scrap_data(url)
-    console.log("TEST:\n" + test);
+    // console.log("TEST:\n" + test);
 }
 
-// main();
+main();
 
 module.exports={scrap_data: scrap_data, get_link: get_link};
 
