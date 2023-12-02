f240b6cb52507a83e268d561ab51b86c2c6226ad
Zachary <75510412+Zachary1575@users.noreply.github.com>
2023-12-01 19:22:54 -0500

    Refactored RSS Feed

--- rssfeed/rss_feed.js
+++ rssfeed/rss_feed.js
@@ -17,20 +17,139 @@ const client = new MongoClient(mongoUrl);
  const url = "https://dummy-server-toswle5frq-uc.a.run.app/upload_csv";
  const proxy_Url = corsProxy + url;
 
-const get_link = async () => {
-    try {
-        await client.connect();
-        let db = client.db(dbName);
-        const rss_links = db.collection("rss_links");
-        // need to change findOne in the future 
-        const rssLink = await rss_links.findOne({url: "https://www.wgbh.org/tags/bunp.rss"});
-        return rssLink.url;
-    } finally {
-        await client.close();
-    }
+ // Scrapping Functions
+
+ // Gets all the links (or documents) from MongoDB RSS_Link Collection
+ const get_links = async (client) => {
+    let db = client.db(dbName);
+    const rss_links = db.collection("rss_links");
+    const documents = await rss_links.find({}).toArray();
+    return documents;
 }
 
-// const url = 'https://www.wgbh.org/tags/bunp.rss';
+const scrap_data_to_db = (client, url) => {
+    let titles = [];
+    let links = [];
+    let descriptions = [];
+    let contents = [];
+    let pubDates = [];
+
+    if (!url) {
+        return;
+    }
+
+    axios.get(url).then(response => {
+        return response
+    })
+    .catch((error) => {
+        console.error('Error fetching data:', error)
+    }).then((response) => {
+        const html = response.data;
+        const $ = cheerio.load(html);
+
+        // Scrapping
+        $('item').each(function() {
+            titles.push($('title', this).text());
+            links.push($('guid', this).text());
+            descriptions.push($('description', this).text());
+            pubDates.push($('pubDate', this).text());
+            contents.push($('content\\:encoded', this).text());
+        });
+
+        const rss_data = client.db(dbName).collection("rss_data");
+
+        // Hashing
+        const ids = contents.map(content => {
+            return crypto.createHash('sha256').update(content).digest('hex');
+        })
+    
+        let arr = titles.map((title, index) => {
+            return {
+                userId: "1",
+                rssLink: "https://www.wgbh.org/tags/bunp.rss",
+                id: ids[index],
+                title: title,
+                link: links[index],
+                description: descriptions[index],
+                pubDates: pubDates[index],
+                contents: contents[index]
+            }
+        });
+
+        // Check if incoming duplicates and only take in non-duplicates.
+        rss_data.find({id : {$in: ids} }).toArray().then(existingDocs => {
+            // duplicate ones
+            const existingIds = new Set(existingDocs.map(doc => doc.id));
+            // new ones after remove duplicates
+            const newDocs = arr.filter(doc => !existingIds.has(doc.id));
+        
+            if (newDocs.length > 0) {
+                return rss_data.insertMany(newDocs);
+            } else {
+                console.log("No new items to add.");
+                return Promise.resolve();
+            }
+        })
+        .catch((error) => {
+            console.error('Error Inserting in MongoDB:', error)
+        })
+        .finally(() => {
+            client.close();
+        });
+        
+    });
+
+    return;
+}
+
+
+// Example usage
+const main = () => {
+    // Instaniate MongoDB
+    client.connect().then((client) => {
+        get_links(client)
+        .catch(err => console.log(`Fetching Links Error: ${err}`))
+        .then((documents) => {
+            for (let i = 0; i < documents.length; i++) {
+                scrap_data_to_db(client, documents[i].url);
+            }
+        });
+    });
+}
+
+main();
+
+
+
+
+// *** Other *** //
+// This function removes duplicates from existing database. We can save it for future
+const removeDuplicates = async () => {
+    await client.connect();
+    let db = client.db(dbName);
+    const rss_data = db.collection("rss_data");
+
+    const duplicates = await rss_data.aggregate([
+        {
+            // Group documents by the 'id' field
+            $group: {
+                _id: "$id", 
+                ids: { $addToSet: "$_id" }, 
+                count: { $sum: 1 } 
+            }
+        },
+        {
+            $match: {
+                count: { $gt: 1 } 
+            }
+        }
+    ]).toArray();
+
+    for (let group of duplicates) {
+        group.ids.shift(); // remove the first element from duplicates -> the one we save in db.
+        await rss_data.deleteMany({ _id: { $in: group.ids } });
+    }
+}
 
 const scrap_data_to_csv = async () => {
     let titles = [];
@@ -103,125 +222,7 @@ const scrap_data_to_csv = async () => {
     return myarr;
 }
 
-const scrap_data_to_db = (url) => {
-    // how to plug in user id ?
-    // const { user } = useContext(Auth0Context)!;
-
-    if (!url) {
-        return;
-    }
-
-    let titles = [];
-    let links = [];
-    let descriptions = [];
-    let contents = [];
-    let pubDates = [];
-
-    axios.get(url).then(response => {
-        return response
-    })
-    .catch((error) => {
-        console.error('Error fetching data:', error)
-    }).then((response) => {
-        const html = response.data;
-        const $ = cheerio.load(html);
-
-        // Scrapping
-        $('item').each(function() {
-            titles.push($('title', this).text());
-            links.push($('guid', this).text());
-            descriptions.push($('description', this).text());
-            pubDates.push($('pubDate', this).text());
-            contents.push($('content\\:encoded', this).text());
-        });
-
-        client.connect().then((client) => {
-            const rss_data = client.db(dbName).collection("rss_data");
-
-            const ids = contents.map(content => {
-                return crypto.createHash('sha256').update(content).digest('hex');
-            })
-        
-            let arr = titles.map((title, index) => {
-                return {
-                    userId: "1",
-                    rssLink: "https://www.wgbh.org/tags/bunp.rss",
-                    id: ids[index],
-                    title: title,
-                    link: links[index],
-                    description: descriptions[index],
-                    pubDates: pubDates[index],
-                    contents: contents[index]
-                }
-            });
-
-            // Check if incoming duplicates and only take in non-duplicates.
-            rss_data.find({id : {$in: ids} }).toArray().then(existingDocs => {
-                // duplicate ones
-                const existingIds = new Set(existingDocs.map(doc => doc.id));
-                // new ones after remove duplicates
-                const newDocs = arr.filter(doc => !existingIds.has(doc.id));
-            
-                if (newDocs.length > 0) {
-                    return rss_data.insertMany(newDocs);
-                } else {
-                    console.log("No new items to add.");
-                    return Promise.resolve();
-                }
-            })
-            .catch((error) => {
-                console.error('Error Inserting in MongoDB:', error)
-            })
-            .finally(() => {
-                client.close();
-            });
-        })  
-    });
-
-    return;
-}
-
-// This function removes duplicates from existing database. We can save it for future
-const removeDuplicates = async () => {
-    await client.connect();
-    let db = client.db(dbName);
-    const rss_data = db.collection("rss_data");
-
-    const duplicates = await rss_data.aggregate([
-        {
-            // Group documents by the 'id' field
-            $group: {
-                _id: "$id", 
-                ids: { $addToSet: "$_id" }, 
-                count: { $sum: 1 } 
-            }
-        },
-        {
-            $match: {
-                count: { $gt: 1 } 
-            }
-        }
-    ]).toArray(); 
-
-    for (let group of duplicates) {
-        group.ids.shift(); // remove the first element from duplicates -> the one we save in db.
-        await rss_data.deleteMany({ _id: { $in: group.ids } });
-    }
-}
-
-
-// Example usage
-const main = async () => {
-    // let url = get_link();
-    // let test = await scrap_data_to_csv(url)
-    const url = 'https://www.wgbh.org/tags/bunp.rss';
-    scrap_data_to_db(url);
-    console.log("Main complete.");
-}
-
-main();
-
-module.exports={scrap_data_to_csv: scrap_data_to_csv, get_link: get_link, scrap_data_to_db: scrap_data_to_db};
+//module.exports={scrap_data_to_csv: scrap_data_to_csv, get_link: get_link, scrap_data_to_db: scrap_data_to_db};
 
 
 
