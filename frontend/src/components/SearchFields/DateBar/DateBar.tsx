d52db909af6dd81f675e456102fde20ede6f6a3e
Malik Baker <78194321+malbaker@users.noreply.github.com>
2023-12-10 17:25:30 -0500

    cleaned up auth0 references

--- frontend/src/components/SearchFields/DateBar/DateBar.tsx
+++ frontend/src/components/SearchFields/DateBar/DateBar.tsx
@@ -11,151 +11,150 @@ import { TractContext } from "../../../contexts/tract_context";
 import { NeighborhoodContext } from "../../../contexts/neighborhood_context";
 import { minDate, maxDate } from "../../../App";
 import { TopicsContext } from "../../../contexts/topics_context";
-import { Auth0Context } from "@auth0/auth0-react";
-
+import { useUser } from "@clerk/clerk-react";
 interface DateFieldProps {
-  title: string;
-  isTopicsPage: boolean;
+	title: string;
+	isTopicsPage: boolean;
 }
 
 const DateField: React.FC<DateFieldProps> = ({ isTopicsPage }) => {
-  const [dateFrom, setdateFrom] = React.useState(minDate);
-  const [dateTo, setDateTo] = React.useState(maxDate);
+	const [dateFrom, setdateFrom] = React.useState(minDate);
+	const [dateTo, setDateTo] = React.useState(maxDate);
 
-  const { topicsMasterList, topic, setTopic } =
-    React.useContext(TopicsContext)!;
-  const { articleData, queryArticleDataType, setShouldRefresh } =
-    React.useContext(ArticleContext)!;
-  const { tractData, queryTractDataType } = React.useContext(TractContext)!;
-  const { neighborhood } = React.useContext(NeighborhoodContext)!;
-  const { user } = useContext(Auth0Context);
+	const { topicsMasterList, topic, setTopic } =
+		React.useContext(TopicsContext)!;
+	const { articleData, queryArticleDataType, setShouldRefresh } =
+		React.useContext(ArticleContext)!;
+	const { tractData, queryTractDataType } = React.useContext(TractContext)!;
+	const { neighborhood } = React.useContext(NeighborhoodContext)!;
+	const { user } = useUser();
 
-  const handleChangeFrom = (d: any) => {
-    setShouldRefresh(true);
-    setdateFrom(d);
-  };
+	const handleChangeFrom = (d: any) => {
+		setShouldRefresh(true);
+		setdateFrom(d);
+	};
 
-  const handleChangeTo = (d: any) => {
-    setShouldRefresh(true);
-    setDateTo(d);
-  };
+	const handleChangeTo = (d: any) => {
+		setShouldRefresh(true);
+		setDateTo(d);
+	};
 
-  React.useEffect(() => {
-    setShouldRefresh(false);
+	React.useEffect(() => {
+		setShouldRefresh(false);
 
-    isTopicsPage
-      ? queryArticleDataType("ARTICLE_BY_LABEL_OR_TOPIC", {
-          dateFrom: parseInt(dateFrom.format("YYYYMMDD")),
-          dateTo: parseInt(dateTo.format("YYYYMMDD")),
-          area: tractData?.tract,
-          labelOrTopic: topic,
-          userId: user?.sub,
-        })
-      : queryArticleDataType("ARTICLE_DATA", {
-          dateFrom: parseInt(dateFrom.format("YYYYMMDD")),
-          dateTo: parseInt(dateTo.format("YYYYMMDD")),
-          area: tractData?.tract,
-          userId: user?.sub,
-        });
-  }, [tractData]);
+		isTopicsPage
+			? queryArticleDataType("ARTICLE_BY_LABEL_OR_TOPIC", {
+					dateFrom: parseInt(dateFrom.format("YYYYMMDD")),
+					dateTo: parseInt(dateTo.format("YYYYMMDD")),
+					area: tractData?.tract,
+					labelOrTopic: topic,
+					userId: user?.id,
+			  })
+			: queryArticleDataType("ARTICLE_DATA", {
+					dateFrom: parseInt(dateFrom.format("YYYYMMDD")),
+					dateTo: parseInt(dateTo.format("YYYYMMDD")),
+					area: tractData?.tract,
+					userId: user?.id,
+			  });
+	}, [tractData]);
 
-  React.useEffect(() => {
-    isTopicsPage
-      ? queryArticleDataType("ARTICLE_BY_LABEL_OR_TOPIC", {
-          dateFrom: parseInt(dateFrom.format("YYYYMMDD")),
-          dateTo: parseInt(dateTo.format("YYYYMMDD")),
-          area: "all",
-          labelOrTopic: topic,
-          userId: user?.sub,
-        })
-      : queryArticleDataType("ARTICLE_DATA", {
-          dateFrom: parseInt(dateFrom.format("YYYYMMDD")),
-          dateTo: parseInt(dateTo.format("YYYYMMDD")),
-          area: tractData?.tract,
-          userId: user?.sub,
-        });
-  }, [dateFrom, dateTo]);
+	React.useEffect(() => {
+		isTopicsPage
+			? queryArticleDataType("ARTICLE_BY_LABEL_OR_TOPIC", {
+					dateFrom: parseInt(dateFrom.format("YYYYMMDD")),
+					dateTo: parseInt(dateTo.format("YYYYMMDD")),
+					area: "all",
+					labelOrTopic: topic,
+					userId: user?.id,
+			  })
+			: queryArticleDataType("ARTICLE_DATA", {
+					dateFrom: parseInt(dateFrom.format("YYYYMMDD")),
+					dateTo: parseInt(dateTo.format("YYYYMMDD")),
+					area: tractData?.tract,
+					userId: user?.id,
+			  });
+	}, [dateFrom, dateTo]);
 
-  return (
-    <>
-      <div style={{ display: "flex" }}>
-        <div>
-          <p className="word1">Start Date</p>
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DesktopDatePicker
-              inputFormat="MM/DD/YYYY"
-              value={dateFrom}
-              onChange={handleChangeFrom}
-              minDate={minDate}
-              maxDate={maxDate}
-              renderInput={(params) => (
-                <TextField
-                  sx={{
-                    width: "97%",
-                    "& .MuiInputBase-root": {
-                      borderRadius: 0.5,
-                      height: 34,
-                    },
-                    "& .MuiOutlinedInput-root": {
-                      "& fieldset": {
-                        borderWidth: 2,
-                        borderColor: "#ccc",
-                      },
-                      "&:hover fieldset": {
-                        borderColor: "#ccc",
-                      },
-                    },
-                    "& .MuiInput-underline:after": {
-                      borderBottomColor: "#ccc",
-                    },
-                  }}
-                  {...params}
-                />
-              )}
-            />
-          </LocalizationProvider>
-        </div>
+	return (
+		<>
+			<div style={{ display: "flex" }}>
+				<div>
+					<p className='word1'>Start Date</p>
+					<LocalizationProvider dateAdapter={AdapterDayjs}>
+						<DesktopDatePicker
+							inputFormat='MM/DD/YYYY'
+							value={dateFrom}
+							onChange={handleChangeFrom}
+							minDate={minDate}
+							maxDate={maxDate}
+							renderInput={(params) => (
+								<TextField
+									sx={{
+										width: "97%",
+										"& .MuiInputBase-root": {
+											borderRadius: 0.5,
+											height: 34,
+										},
+										"& .MuiOutlinedInput-root": {
+											"& fieldset": {
+												borderWidth: 2,
+												borderColor: "#ccc",
+											},
+											"&:hover fieldset": {
+												borderColor: "#ccc",
+											},
+										},
+										"& .MuiInput-underline:after": {
+											borderBottomColor: "#ccc",
+										},
+									}}
+									{...params}
+								/>
+							)}
+						/>
+					</LocalizationProvider>
+				</div>
 
-        <div>
-          <p className="word1">End Date</p>
+				<div>
+					<p className='word1'>End Date</p>
 
-          <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DesktopDatePicker
-              inputFormat="MM/DD/YYYY"
-              value={dateTo}
-              onChange={handleChangeTo}
-              minDate={minDate}
-              maxDate={maxDate}
-              renderInput={(params) => (
-                <TextField
-                  sx={{
-                    width: "97%",
-                    "& .MuiInputBase-root": {
-                      borderRadius: 0.5,
-                      height: 34,
-                    },
-                    "& .MuiOutlinedInput-root": {
-                      "& fieldset": {
-                        borderWidth: 2,
-                        borderColor: "#ccc",
-                      },
-                      "&:hover fieldset": {
-                        borderColor: "#ccc",
-                      },
-                    },
-                    "& .MuiInput-underline:after": {
-                      borderBottomColor: "#ccc",
-                    },
-                  }}
-                  {...params}
-                />
-              )}
-            />
-          </LocalizationProvider>
-        </div>
-      </div>
-    </>
-  );
+					<LocalizationProvider dateAdapter={AdapterDayjs}>
+						<DesktopDatePicker
+							inputFormat='MM/DD/YYYY'
+							value={dateTo}
+							onChange={handleChangeTo}
+							minDate={minDate}
+							maxDate={maxDate}
+							renderInput={(params) => (
+								<TextField
+									sx={{
+										width: "97%",
+										"& .MuiInputBase-root": {
+											borderRadius: 0.5,
+											height: 34,
+										},
+										"& .MuiOutlinedInput-root": {
+											"& fieldset": {
+												borderWidth: 2,
+												borderColor: "#ccc",
+											},
+											"&:hover fieldset": {
+												borderColor: "#ccc",
+											},
+										},
+										"& .MuiInput-underline:after": {
+											borderBottomColor: "#ccc",
+										},
+									}}
+									{...params}
+								/>
+							)}
+						/>
+					</LocalizationProvider>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default DateField;
--- frontend/src/pages/TopicsPage/TopicsPage/TopicsPage.tsx
+++ frontend/src/pages/TopicsPage/TopicsPage/TopicsPage.tsx
@@ -24,214 +24,229 @@ import { TopicsContext } from "../../../contexts/topics_context";
 import { useNavigate } from "react-router-dom";
 import DateField from "../../../components/SearchFields/DateBar/DateBar";
 import { maxDate, minDate } from "../../../App";
-import { useAuth0 } from "@auth0/auth0-react";
+import { useUser } from "@clerk/clerk-react";
 
 function getNeighborhood(
-  code: string,
-  neighborhoods: { [key: string]: string[] }
+	code: string,
+	neighborhoods: { [key: string]: string[] },
 ): string | null {
-  for (let [neighborhoodName, codes] of Object.entries(neighborhoods)) {
-    if (codes.includes(code)) {
-      return neighborhoodName;
-    }
-  }
-  return "";
+	for (let [neighborhoodName, codes] of Object.entries(neighborhoods)) {
+		if (codes.includes(code)) {
+			return neighborhoodName;
+		}
+	}
+	return "";
 }
 
 function countArticlesByKeyWord(
-  articles: Article[],
-  positionSection: string,
-  listOfTopics: string[],
-  neighborhoods: { [key: string]: string[] }
+	articles: Article[],
+	positionSection: string,
+	listOfTopics: string[],
+	neighborhoods: { [key: string]: string[] },
 ) {
-  let counts: any = {};
-  articles.forEach((article) => {
-    if (listOfTopics.indexOf(positionSection) == -1) {
-      if (article.openai_labels[0] === positionSection && article.tracts) {
-        article.tracts.forEach((tract) => {
-          counts[tract] = (counts[tract] || 0) + 1;
-        });
-      }
-    } else {
-      if (article.position_section === positionSection && article.tracts) {
-        article.tracts.forEach((tract) => {
-          counts[tract] = (counts[tract] || 0) + 1;
-        });
-      }
-    }
-  });
+	let counts: any = {};
+	articles.forEach((article) => {
+		if (listOfTopics.indexOf(positionSection) == -1) {
+			if (
+				article.openai_labels[0] === positionSection &&
+				article.tracts
+			) {
+				article.tracts.forEach((tract) => {
+					counts[tract] = (counts[tract] || 0) + 1;
+				});
+			}
+		} else {
+			if (
+				article.position_section === positionSection &&
+				article.tracts
+			) {
+				article.tracts.forEach((tract) => {
+					counts[tract] = (counts[tract] || 0) + 1;
+				});
+			}
+		}
+	});
 
-  let sortedCounts = Object.entries(counts).sort(
-    (a: any, b: any) => b[1] - a[1]
-  );
-  return getDisplayTractList(sortedCounts, neighborhoods);
+	let sortedCounts = Object.entries(counts).sort(
+		(a: any, b: any) => b[1] - a[1],
+	);
+	return getDisplayTractList(sortedCounts, neighborhoods);
 }
 
 function getDisplayTractList(
-  countOfTract: [string, unknown][],
-  neighborhoods: { [key: string]: string[] }
+	countOfTract: [string, unknown][],
+	neighborhoods: { [key: string]: string[] },
 ) {
-  const result: string[] = [];
+	const result: string[] = [];
 
-  countOfTract.forEach((element) => {
-    result.push(
-      `${getNeighborhood(element[0], neighborhoods)} - ${element[0]} - ${
-        element[1]
-      }`
-    );
-  });
+	countOfTract.forEach((element) => {
+		result.push(
+			`${getNeighborhood(element[0], neighborhoods)} - ${element[0]} - ${
+				element[1]
+			}`,
+		);
+	});
 
-  return result;
+	return result;
 }
 
 function extractNeighborhoodTract(text: string) {
-  const match = /([\w\s]+ - )?(\d+)/.exec(text);
-  let location = "";
-  let number = "";
+	const match = /([\w\s]+ - )?(\d+)/.exec(text);
+	let location = "";
+	let number = "";
 
-  if (match) {
-    location = match[1] ? match[1].slice(0, -3) : ""; // Remove trailing ' - ' from the location
-    number = match[2];
-  }
+	if (match) {
+		location = match[1] ? match[1].slice(0, -3) : ""; // Remove trailing ' - ' from the location
+		number = match[2];
+	}
 
-  return [location, number];
+	return [location, number];
 }
 
 const TopicsPage: React.FC = () => {
-  const navigate = useNavigate(); // Initialize useNavigate hook
+	const navigate = useNavigate(); // Initialize useNavigate hook
 
-  function handleBoxClick() {
-    navigate("../"); // Navigate to the new route
-  }
+	function handleBoxClick() {
+		navigate("../"); // Navigate to the new route
+	}
 
-  //Contex
-  const { articleData, queryArticleDataType, setShouldRefresh, shouldRefresh } =
-    React.useContext(ArticleContext)!;
-  const { tractData, queryTractDataType } = React.useContext(TractContext)!;
-  const { neighborhoodMasterList, setNeighborhood, neighborhood } =
-    React.useContext(NeighborhoodContext)!;
-  const { topicsMasterList, topic, setTopic } =
-    React.useContext(TopicsContext)!;
-  const { user, isAuthenticated } = useAuth0();
+	//Contex
+	const {
+		articleData,
+		queryArticleDataType,
+		setShouldRefresh,
+		shouldRefresh,
+	} = React.useContext(ArticleContext)!;
+	const { tractData, queryTractDataType } = React.useContext(TractContext)!;
+	const { neighborhoodMasterList, setNeighborhood, neighborhood } =
+		React.useContext(NeighborhoodContext)!;
+	const { topicsMasterList, topic, setTopic } =
+		React.useContext(TopicsContext)!;
+	const { user } = useUser();
 
-  //State
-  const [tracts, setTracts] = React.useState<string[]>([]);
-  const [flag, setFlag] = React.useState(true);
-  const [currentTopic, setcurrentTopic] = React.useState("");
+	//State
+	const [tracts, setTracts] = React.useState<string[]>([]);
+	const [flag, setFlag] = React.useState(true);
+	const [currentTopic, setcurrentTopic] = React.useState("");
 
-  // Setting Default Values
-  React.useEffect(() => {
-    if (topic) {
-      setShouldRefresh(true);
-      queryArticleDataType("ARTICLE_BY_LABEL_OR_TOPIC", {
-        dateFrom: parseInt(minDate.format("YYYYMMDD")),
-        dateTo: parseInt(maxDate.format("YYYYMMDD")),
-        area: "all",
-        labelOrTopic: topic,
-        userId: user?.sub,
-      });
-    }
-  }, [topic]);
+	// Setting Default Values
+	React.useEffect(() => {
+		if (topic) {
+			setShouldRefresh(true);
+			queryArticleDataType("ARTICLE_BY_LABEL_OR_TOPIC", {
+				dateFrom: parseInt(minDate.format("YYYYMMDD")),
+				dateTo: parseInt(maxDate.format("YYYYMMDD")),
+				area: "all",
+				labelOrTopic: topic,
+				userId: user?.id,
+			});
+		}
+	}, [topic]);
 
-  //Set deafult count and list
-  React.useEffect(() => {
-    // console.log(articleData, shouldRefresh);
+	//Set deafult count and list
+	React.useEffect(() => {
+		// console.log(articleData, shouldRefresh);
 
-    if (articleData && shouldRefresh) {
-      const countTemp = countArticlesByKeyWord(
-        articleData!,
-        topic!,
-        topicsMasterList!,
-        neighborhoodMasterList!
-      );
+		if (articleData && shouldRefresh) {
+			const countTemp = countArticlesByKeyWord(
+				articleData!,
+				topic!,
+				topicsMasterList!,
+				neighborhoodMasterList!,
+			);
 
-      const extra = extractNeighborhoodTract(countTemp[0]);
+			const extra = extractNeighborhoodTract(countTemp[0]);
 
-      queryTractDataType("TRACT_DATA", {
-        tract: extra[1],
-      });
+			queryTractDataType("TRACT_DATA", {
+				tract: extra[1],
+			});
 
-      setNeighborhood(extra[0]);
-      setTracts(countTemp);
-    }
-  }, [articleData, shouldRefresh]);
+			setNeighborhood(extra[0]);
+			setTracts(countTemp);
+		}
+	}, [articleData, shouldRefresh]);
 
-  return (
-    <>
-      {!(
-        articleData &&
-        topic &&
-        neighborhood &&
-        tractData &&
-        neighborhoodMasterList
-      ) ? (
-        <Stack
-          sx={{
-            width: "100%",
-            color: "grey.500",
-            marginTop: "10px",
-          }}
-          spacing={2}
-        >
-          <LinearProgress color="secondary" />
-        </Stack>
-      ) : (
-        <div className="big-container">
-          <div className="row justify-content-between">
-            <div className="col-md-9 col-sm-12">
-              <div
-                className="align-self-start org-back"
-                onClick={handleBoxClick}
-              >
-                {" "}
-                <i
-                  className="fa fa-arrow-left"
-                  aria-hidden="true"
-                  style={{ marginRight: "10px" }}
-                ></i>
-                Back to Search Page
-              </div>
+	return (
+		<>
+			{!(
+				articleData &&
+				topic &&
+				neighborhood &&
+				tractData &&
+				neighborhoodMasterList
+			) ? (
+				<Stack
+					sx={{
+						width: "100%",
+						color: "grey.500",
+						marginTop: "10px",
+					}}
+					spacing={2}
+				>
+					<LinearProgress color='secondary' />
+				</Stack>
+			) : (
+				<div className='big-container'>
+					<div className='row justify-content-between'>
+						<div className='col-md-9 col-sm-12'>
+							<div
+								className='align-self-start org-back'
+								onClick={handleBoxClick}
+							>
+								{" "}
+								<i
+									className='fa fa-arrow-left'
+									aria-hidden='true'
+									style={{ marginRight: "10px" }}
+								></i>
+								Back to Search Page
+							</div>
 
-              <div className="align-self-start your-org">SELECTED TOPIC</div>
-              <div className="align-self-start org-name">
-                {topic == null ? "No Topic Selected" : topic}
-              </div>
-              <h1></h1>
-            </div>
+							<div className='align-self-start your-org'>
+								SELECTED TOPIC
+							</div>
+							<div className='align-self-start org-name'>
+								{topic == null ? "No Topic Selected" : topic}
+							</div>
+							<h1></h1>
+						</div>
 
-            <div className="col-md-3 col-sm-12">
-              <div>
-                <DateField title="From" isTopicsPage={true}></DateField>
-              </div>
-            </div>
-          </div>
+						<div className='col-md-3 col-sm-12'>
+							<div>
+								<DateField
+									title='From'
+									isTopicsPage={true}
+								></DateField>
+							</div>
+						</div>
+					</div>
 
-          <div className="row justify-content-evenly">
-            <div className="col-md-5 col-sm-12">
-              <h1 className="titles">Tracts</h1>
-              <TractsDropDown tracts={tracts}></TractsDropDown>
-            </div>
-            <div className="col-md-7 col-sm-12">
-              <h1 className="titles">Map</h1>
-              <MapCard clickable={false}></MapCard>
-            </div>
-          </div>
+					<div className='row justify-content-evenly'>
+						<div className='col-md-5 col-sm-12'>
+							<h1 className='titles'>Tracts</h1>
+							<TractsDropDown tracts={tracts}></TractsDropDown>
+						</div>
+						<div className='col-md-7 col-sm-12'>
+							<h1 className='titles'>Map</h1>
+							<MapCard clickable={false}></MapCard>
+						</div>
+					</div>
 
-          <div className="row justify-content-evenly">
-            <div className="col-md-5 col-sm-12">
-              <h1 className="titles">Demographics</h1>
-              <NeighborhoodDemographicsBoard></NeighborhoodDemographicsBoard>
-            </div>
-            <div className="col-md-7 col-sm-12">
-              <h1 className="titles">Articles</h1>
+					<div className='row justify-content-evenly'>
+						<div className='col-md-5 col-sm-12'>
+							<h1 className='titles'>Demographics</h1>
+							<NeighborhoodDemographicsBoard></NeighborhoodDemographicsBoard>
+						</div>
+						<div className='col-md-7 col-sm-12'>
+							<h1 className='titles'>Articles</h1>
 
-              <ArticleCard></ArticleCard>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
+							<ArticleCard></ArticleCard>
+						</div>
+					</div>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default TopicsPage;
