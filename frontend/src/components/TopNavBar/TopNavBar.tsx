855ad2e777d9703ebb25a89dc60cfe590ba5bb8d
Olga Vasilaki <your_email@example.com>
2024-04-19 14:59:09 -0400

    Added demo permission

--- frontend/src/components/TopNavBar/TopNavBar.tsx
+++ frontend/src/components/TopNavBar/TopNavBar.tsx
@@ -1,27 +1,22 @@
 import { Layout, Button, Anchor } from "antd"; // Ant Design
-import { useNavigate, NavigateFunction, Link, NavLink } from "react-router-dom";
-import Logo from "../../assets/logos/logo.svg";
-import {
-	OrganizationSwitcher,
-	SignedIn,
-	SignedOut,
-	UserButton,
-	useOrganization,
-	useUser,
-} from "@clerk/clerk-react";
-// CSS
-import "./TopNavBar.css";
-import { useState } from "react";
-
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { OrganizationSwitcher, SignedIn, UserButton, useUser, useOrganization, useAuth } from '@clerk/clerk-react';
+import Logo from '../../assets/logos/logo.svg';
+import './TopNavBar.css';
 const { Header } = Layout;
 
 const TopNavBar = () => {
-	const [menuOpen, setMenuOpen] = useState(false);
-	const { user } = useUser();
-	const { organization } = useOrganization();
-	const currentUserOrg = user?.organizationMemberships.find(
-		(ele) => ele.organization.id === organization?.id,
-	);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const { user } = useUser();
+    const { organization } = useOrganization();
+    const { has } = useAuth();  
+    const currentUserOrg = user?.organizationMemberships.find(
+        (ele) => ele.organization.id === organization?.id,
+    );
+
+    // Permission check for demo
+    const canUpload = has ? !has({ permission: "org:test:demo" }) : true;
 
 	return (
 		<nav>
@@ -48,26 +43,27 @@ const TopNavBar = () => {
 				<li>
 					<NavLink to='/'>Neighborhoods</NavLink>
 				</li>
-				{/* <li>
-          <NavLink to="/Comparison">Comparison</NavLink>
-        </li> */}
-				<li>
-					<NavLink to='/Upload'>Upload</NavLink>
-				</li>
-				<li>
-					<NavLink to='/Dashboard'>Dashboard</NavLink>
-				</li>
-				<li>
-					<OrganizationSwitcher hidePersonal={true} />
-				</li>
-				<li>
-					<SignedIn>
-						<UserButton />
-					</SignedIn>
-				</li>
-			</ul>
-		</nav>
-	);
+                {canUpload && (
+                    <li>
+                        <NavLink to='/Upload'>Upload</NavLink>
+                    </li>
+                )}
+				{canUpload && (
+                    <li>
+                        <NavLink to='/Dashboard'>Dashboard</NavLink>
+                    </li>
+                )}
+                <li>
+                    <OrganizationSwitcher hidePersonal={true} />
+                </li>
+                <li>
+                    <SignedIn>
+                        <UserButton />
+                    </SignedIn>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default TopNavBar;
