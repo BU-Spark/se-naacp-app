3129119000cd606da22dbe2fd4bdd1f1a629b0fa
lixi <lxi1@binghamton.edu>
2024-05-02 12:47:21 -0400

    add test template for backend

---
+++ backend/graphql/resolvers/graphql_resolvers.test.ts
@@ -0,0 +1,8 @@
+// Add test
+describe('Component or Module Name', () => {
+    it('should do something', () => {
+      // TODO: Add tests
+      // expect(true).toBe(false); // Change this once test is implemented
+    });
+  });
+  
