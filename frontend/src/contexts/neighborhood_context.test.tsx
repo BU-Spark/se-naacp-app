e381cb01c7017305e33a5d02e6c56866e382ee12
lixi <lxi1@binghamton.edu>
2024-05-02 12:53:47 -0400

    add frontend test template

--- frontend/src/contexts/neighborhood_context.test.tsx
+++ frontend/src/contexts/neighborhood_context.test.tsx
@@ -1,2 +1,10 @@
 import { render, fireEvent, screen } from '@testing-library/react';
 import NeighborhoodProvider from './neighborhood_context';
+
+// Modify this template to test context
+describe('YourContext', () => {
+    it('Do something', () => {
+      // Add more tests here
+    });
+  });
+  
