796e06b88be3475e6233b543711201d7e128e566
Nikhil Ramchandani <ramchandaninikhil01@gmail.com>
2024-09-18 20:11:06 -0400

    new workflow

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,8 @@ module.exports = {
       '^.+\\.(js|jsx|ts|tsx)$': 'babel-jest',
     },
     transformIgnorePatterns: [
-      'node_modules/(?!(axios|@mui|x-charts|d3-shape)/)', // Explicitly transform certain node_modules
+      'node_modules/(?!(axios|@mui|x-charts|d3-shape|use-supercluster)/)',      
       '/node_modules/(?!@mui/x-charts|@mui/material|@babel/runtime|d3-(color|format|interpolate|scale|shape|time|time-format|path|array)|internmap)',
-      'node_modules/(?!(use-supercluster)/)', 
-
       
     ],
     moduleNameMapper: {
