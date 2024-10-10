0f08a2315d372f62b8c6dd62683f316ac0f783a1
Raheeq Ibrahim <113067381+raheeqi@users.noreply.github.com>
2024-10-10 15:09:08 -0400

    Update authMiddleware.ts

--- backend/authMiddleware.ts
+++ backend/authMiddleware.ts
@@ -17,13 +17,6 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     throw new Error("Internal Server Error");
   }
 
-  // Extract the token and organization token from request headers
-  //const token = req.headers['x-org-token'];
- //const token = 'eyJhbGciOiJSUzI1NiIsImNhdCI6ImNsX0I3ZDRQRDIyMkFBQSIsImtpZCI6Imluc18yWkdwcmlDckozQWM5dmh0OTZJY2VJMDBQT0IiLCJ0eXAiOiJKV1QifQ.eyJhenAiOiJodHRwOi8vbG9jYWxob3N0OjMwMDAiLCJleHAiOjE3Mjg1MDk5ODIsImlhdCI6MTcyODQ5MTk4MiwiaXNzIjoiaHR0cHM6Ly9kYXJpbmctY3JheWZpc2gtMjEuY2xlcmsuYWNjb3VudHMuZGV2IiwianRpIjoiMjJlMGE0MzU3YjI2MDIzMjQzOTMiLCJuYmYiOjE3Mjg0OTE5NzcsIm9yZ3MiOiJvcmdfMmJIRHpsMlpheDBuSUxJekRodWkyRExXZEg2Iiwic3ViIjoidXNlcl8ybHllMlI2OVI5T2Z2TW9uRlB3ZXNBVkdKazMifQ.mWTc7_QaWBy2N_T1U0my7Mi2WTLStEwEsL1plrJ3u1d3C_O8sSz3_X9_-L6etqC_qvC_JO_xBIiXRRQN_eBPvU6xrFUk80dE6oJkvMIsKyFKms63Zt7KZVsa41XFUfuixNQo4qd0i87ZaQ37uEEFG6e0O-UjeknEimoeNLPaRnZSw8W8Ea2YA5PVKxvPPR_9oLEoJq-U_WzdLGqZBME3-qjoMIS6DmoTEnr3g68dVk_d9i7mBiCIjeKkU2_PPJaiD30j78CWB-KpONS9Gjxznre8ERbz-gaNXRmdzf0IKMX5kxZChI3eSF5MS-Cn0MN1JT4NS-1h8QbzPSPWca4rVQ';
-  //console.log("headers", req.headers)
-  //console.log("token", token)
-  // const orgToken = req.headers['x-org-token'];
-  // Check if token or organization token is missing
   if (!token) {
     const error = new Error("Unauthorized");
     (error as any).code = 401;
