132adfec104cc122adb42c2b2088d4f2803be3e5
Nikhil Ramchandani <ramchandaninikhil01@gmail.com>
2024-10-16 23:25:47 -0400

    topic filtering locations page and new topic filter

--- frontend/src/components/LocationsInfo/LocationsInfo.tsx
+++ frontend/src/components/LocationsInfo/LocationsInfo.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import { Locations } from "../../__generated__/graphql";
 import { Card, CardContent } from '@mui/material';
+import { filter } from 'lodash';
 
 
 interface LocationInfoProps {
     location: Locations | null;
+    filteredArticlesLength: number;
 }
 
-const LocationInfo: React.FC<LocationInfoProps> = ({ location }) => {
+const LocationInfo: React.FC<LocationInfoProps> = ({ location, filteredArticlesLength }) => {
     if (!location) {
         return <div>No location selected</div>;
     }
 
     return (
 
-        <Card sx={{ width: "100%", height: "62vh" }}> 
+        <Card sx={{ width: "100%", height: "30vh" }}> 
             <CardContent sx={{ width: "100%", height: "62vh"}} >
                 <div>
                     <div><h2>Location Information</h2></div>
@@ -30,6 +32,13 @@ const LocationInfo: React.FC<LocationInfoProps> = ({ location }) => {
                     { location.tract && (
                     <div><strong>Tract:</strong> {location.tract}</div>
                     )}
+                    { location.articles && (
+                        <div><strong>Article Count:</strong> {location.articles.length}</div>
+                    )}
+                    { filteredArticlesLength !== location.articles.length && (
+                        <div><strong>Filtered Article Count:</strong> {filteredArticlesLength}</div>
+                    )}
+
                 </div>
             </CardContent>
         </Card> 
